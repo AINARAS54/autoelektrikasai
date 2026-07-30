e3179e8456d69b7bf5ba21ea141f268e2f6a6a98
AINARAS54 <ainaraskalnenas@gmail.com>
2026-07-30 13:20:51 +0200

    vehicleprofile

---
+++ vehicle_profile_engine.py
@@ -0,0 +1,15 @@
+{
+  "brand": "BMW",
+  "model": "i3",
+  "year": 2019,
+  "platform": "EV",
+  "engine": "Electric",
+  "has_hv_battery": true,
+  "has_12v_battery": true,
+  "has_starter": false,
+  "has_alternator": false,
+  "has_spark_plugs": false,
+  "has_injectors": false,
+  "has_dpf": false,
+  "has_turbo": false
+}
